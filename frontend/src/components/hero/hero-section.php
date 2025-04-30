6e5c525d8145424bbeede444190a3db8815a8dd9
Vincent Rivera <vincentoloteorivera@gmail.com>
2025-04-30 18:44:25 +0800

    Changes Reusable Left Div

---
+++ frontend/src/components/hero/hero-section.php
@@ -0,0 +1,12 @@
+<div class="leftDiv">
+    <div class="image-container">
+        <img src="frontend/src/assets/images/weak-hero.jpg" alt="image">
+        <div class="overlay"></div>
+        <div class="text-overlay">
+            <h1>Welcome to<br>BEACompanion!👋</h1>
+            <p>Understand behavioral and speech patterns <b>like never before!</b> Gain deep insights into ADHD
+                assessment
+                through intelligent monitoring and analysis—helping children thrive with confidence.</p>
+        </div>
+    </div>
+</div>
--- frontend/src/pages/sign-up/sign-up.php
+++ frontend/src/pages/sign-up/sign-up.php
@@ -1,17 +1,7 @@
 <div id="sign-up">
+
     <!-- Left Container -->
-    <div class="leftDiv">
-        <div class="image-container">
-            <img src="frontend/src/assets/images/weak-hero.jpg" alt="image">
-            <div class="overlay"></div>
-            <div class="text-overlay">
-                <h1>Welcome to<br>BEACompanion!👋</h1>
-                <p>Understand behavioral and speech patterns <b>like never before!</b> Gain deep insights into ADHD
-                    assessment
-                    through intelligent monitoring and analysis—helping children thrive with confidence.</p>
-            </div>
-        </div>
-    </div>
+    <?php include '../../components/hero/hero-section.php'; ?>
 
     <!-- Right Container -->
     <div class="rightDiv">
