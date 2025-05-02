f44108a355973efb6e4cc1cd9153198916b8ad0c
Vincent Rivera <vincentoloteorivera@gmail.com>
2025-05-03 01:59:14 +0800

    Adjustment w/ GSAP

--- frontend/src/pages/forgot-password/forgot-password.php
+++ frontend/src/pages/forgot-password/forgot-password.php
@@ -1,4 +1,4 @@
-<div id="forgot-password">
+<div id="forgot-password-page">
 
     <!-- Left Container -->
     <?php include '../../components/hero/hero-section.php'; ?>
@@ -6,7 +6,26 @@
     <!-- Right Container -->
     <div class="rightDiv">
 
-        <h1>Hello World</h1>
+        <span class="user-back" id="backBtnWrapper">
+            <a href="javascript:void(0)" onclick="loadPage('loginPage')" id="backBtn">
+                <i class="fa-solid fa-arrow-left"></i>
+            </a>
+            <span class="back-text">Back</span>
+        </span>
+
+        <div class="company-name">
+            <h2>BEACompanion</h2>
+        </div>
+
+        <div class="user-forgot-password">
+            <h1>Forgot Password</h1>
+            <p class="paragraph-fade">Please enter the email address you'd like your password reset information sent to
+            </p>
+        </div>
+
+
+
+        <h1>Task for 10 a.m.</h1>
     </div>
 
 </div>
